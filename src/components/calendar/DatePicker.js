b54182c6a86b452056db48dad87a7187fde456ec
JCEleanor <crcky4826@gmail.com>
2021-07-07 22:22:13 +0800

    fixes #1

--- src/components/calendar/DatePicker.js
+++ src/components/calendar/DatePicker.js
@@ -6,14 +6,13 @@ function DatePicker(props) {
   // selectedDate: default value = new Date()
   const { selectedDate, setSelectedDate } = props
 
-
   return (
     <Calendar
       onChange={setSelectedDate}
       // value={date}
       defaultValue={selectedDate}
-      locale={"en"}
-      minDate={selectedDate}
+      locale={'en'}
+      minDate={new Date()}
       next2Label={null}
       prev2Label={null}
     />
