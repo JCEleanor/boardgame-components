ae63b9cd2c8e3dc6e3432e3e8b93f455adb8cb36
JCEleanor <crcky4826@gmail.com>
2021-07-15 17:59:18 +0800

    todo: convert to functional component

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
+// todo: convert to functional component
 class GoogleAuth extends React.Component {
   state = { isSignedIn: null }
 
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client
+        // params doc: https://developers.google.com/identity/sign-in/web/reference?hl=en#gapiauth2initparams
         .init({
-          clientId:
+          client_id:
             '1054065807793-5p0rkni1fj0ktvlp9l5e7ef3dp5pdon9.apps.googleusercontent.com',
+          ux_mode: 'popup',
+          // access users' email
           scope: 'email',
         })
         .then(() => {
@@ -26,13 +30,58 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() })
   }
 
+  // sign in
+  onLogInClick = () => {
+    this.auth.signIn()
+    const currentUserId = window.gapi.auth2
+      .getAuthInstance()
+      .currentUser.get()
+      .getBasicProfile()
+      .getId()
+    const currentUserMail = window.gapi.auth2
+      .getAuthInstance()
+      .currentUser.get()
+      .getBasicProfile()
+      .getEmail()
+    const currentUseName = window.gapi.auth2
+      .getAuthInstance()
+      .currentUser.get()
+      .getBasicProfile()
+      .getName()
+
+    localStorage.setItem('userId', currentUserId)
+    localStorage.setItem('userMail', currentUserMail)
+    localStorage.setItem('username', currentUseName)
+  }
+  // register a user
+  onSignUpClick = () => {}
+
+  // sing out
+  onLogOutClick = () => {
+    this.auth.signOut()
+    localStorage.clear()
+  }
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>status uncertain</div>
+      return null
     } else if (this.state.isSignedIn) {
-      return <div>signed in!</div>
+      return (
+        <button onClick={this.onLogOutClick} className="btn btn-info">
+          log out
+        </button>
+      )
     } else {
-      return <div>not signed in</div>
+      return (
+        <>
+          <button onClick={this.onLogInClick} className="btn btn-success">
+            log in
+          </button>
+          <button onClick={this.onSignUpClick} className="btn btn-primary">
+            sign up
+          </button>
+        </>
+      )
     }
   }
 
