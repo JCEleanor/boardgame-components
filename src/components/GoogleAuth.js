f33e02306660a1e9d51d13286ab53cb43c465ac2
JCEleanor <crcky4826@gmail.com>
2021-07-16 17:35:51 +0800

    add navbar from bs, login succes

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,17 +2,21 @@ import React from 'react'
 import GoogleLogin from 'react-google-login'
 
 const responseGoogle = (response) => {
-  console.log(response)
+  const { googleId, email, ImageUrl, givenName } = response.profileObj
+  console.log(response.profileObj)
 }
 
 const GoogleAuth = () => {
-  ;<GoogleLogin
-    clientId="1054065807793-5p0rkni1fj0ktvlp9l5e7ef3dp5pdon9.apps.googleusercontent.com"
-    buttonText="Login"
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
-    cookiePolicy={'single_host_origin'}
-  />
+  return (
+    <GoogleLogin
+      clientId="1054065807793-5p0rkni1fj0ktvlp9l5e7ef3dp5pdon9.apps.googleusercontent.com"
+      buttonText="Login with Google"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      isSignedIn={true}
+      cookiePolicy={'single_host_origin'}
+    />
+  )
 }
 
 export default GoogleAuth
